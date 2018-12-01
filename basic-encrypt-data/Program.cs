0dd24ffc6059eef29ce5f27ab887c467ef446f26
felipebossolani <fbossolani@gmail.com>
2018-12-01 16:13:57 -0200

    Add project files.

---
+++ basic-encrypt-data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_encrypt_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
